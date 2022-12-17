b926b4eb82ac66af8ccbe3359c3582a0cd663fa9
Vitalii Spivak <vvs140374@gmail.com>
2022-12-17 23:41:06 +0200

    new-file08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
+// Change code below this line
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const galleryCards = creatGalaryCards(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', galleryCards);
+
+galleryRef.addEventListener('click', onImgSmallClick);
+
+function creatGalaryCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <a class="gallery__item" href="${original}">
+               <img
+                class="gallery__image"
+                src="${preview}" 
+                alt="${description}"
+                />
+            </a>
+      `;
+    })
+    .join('');
+}
+
+function onImgSmallClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
